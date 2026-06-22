4f147b7bc3b6c35dd3fe1b0a7f8bc45ce51dbe37
Hifnie Nazih <hifninazih111@gmail.com>
2026-06-22 11:23:26 +0700

    feat: add redirect for /uji-usabilitas in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/uji-usabilitas",
+        destination: "https://forms.gle/MadtY7SHRXj547q38",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
